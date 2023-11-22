12e425ccf59141f1d041b730baea9a226c713921
Claudiu Tarta <claudiu.tarta@gmail.com>
2023-11-22 11:29:21 +0100

    With further extending of the test cases we are revealing that our calculator cannot even return the prices for single items so this means we need to be able to retrieve price information for our products

--- spec/support/price_calculator_spec.rb
+++ spec/support/price_calculator_spec.rb
@@ -3,6 +3,27 @@ require "rails_helper"
 RSpec.describe PriceCalculator do
   let(:calculator) { described_class.new(items: items) }
   describe "#total_price" do
+    context "When the items in the cart are: GR1" do
+      let(:items) { ["GR1"] }
+      it "returns 3.11€" do
+        expect(calculator.total_price).to eq "3.11€"
+      end 
+    end
+    
+    context "When the items in the cart are: SR1" do
+      let(:items) { ["SR1"] }
+      it "returns 5.00€" do
+        expect(calculator.total_price).to eq "5.00€"
+      end 
+    end
+    
+    context "When the items in the cart are: CF1" do
+      let(:items) { ["CF1"] }
+      it "returns 11.23€" do
+        expect(calculator.total_price).to eq "11.23€"
+      end 
+    end
+
     context "When the items in the cart are: GR1, GR1" do
       let(:items) { ["GR1", "GR1"] }
       it "returns 3.11€" do
