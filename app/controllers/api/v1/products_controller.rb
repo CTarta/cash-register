63954854c8765150b235b93b5b68aa54597d5b1d
Claudiu Tarta <claudiu.tarta@gmail.com>
2023-11-22 10:26:37 +0100

    Defined the required route and created associated controller and action to respond to the request to get the total price for the products in the cart

---
+++ app/controllers/api/v1/products_controller.rb
@@ -0,0 +1,5 @@
+class Api::V1::ProductsController < ApplicationController
+
+  def total_price
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api do
+    namespace :v1 do
+      resources :products, only: [] do
+        get :total_price, on: :collection
+      end
+    end
+  end
 end
