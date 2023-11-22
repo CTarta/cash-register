64e7003d1775da9b40e24c9be3fa5337abd3b541
Claudiu Tarta <claudiu.tarta@gmail.com>
2023-11-22 11:13:42 +0100

    Outlining the expected behaviour for the PriceCalculator#total_price as identified by the current set of needs

---
+++ spec/support/price_calculator_spec.rb
@@ -0,0 +1,34 @@
+require "rails_helper"
+
+RSpec.describe PriceCalculator do
+  let(:calculator) { described_class.new(items: items) }
+  describe "#total_price" do
+    context "When the items in the cart are: GR1, GR1" do
+      let(:items) { ["GR1", "GR1"] }
+      it "returns 3.11€" do
+        expect(calculator.total_price).to eq "3.11€"
+      end 
+    end
+
+    context "When the items in the cart are: SR1, SR1, GR1, SR1" do
+      let(:items) { ["SR1", "SR1", "GR1", "SR1"] }
+      it "returns 16.61€" do
+        expect(calculator.total_price).to eq "16.61€"
+      end   
+    end
+
+    context "When the items in the cart are: GR1, CF1, SR1, CF1, CF1" do
+      let(:items) { ["GR1", "CF1", "SR1", "CF1", "CF1"] }
+      it "returns 30.57€" do
+        expect(calculator.total_price).to eq "30.57€"
+      end 
+    end
+
+    context "When there are no items in the cart" do
+      let(:items) { [] }
+      it "returns 0.00€" do
+        expect(calculator.total_price).to eq "0.00€"
+      end 
+    end
+  end
+end
