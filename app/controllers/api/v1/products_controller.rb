c6a1ca804d3f6e15edccca7b5a7a7e4d84eb079d
Claudiu Tarta <claudiu.tarta@gmail.com>
2023-11-22 11:12:02 +0100

    Refactoring ProductsController#total_price in line with our previous decision follow the single responsibility principle and outlining the desired interface for a new class that we shall call PriceCalculator for now. The name of the class is inspired by its responsibility

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -2,12 +2,8 @@ class Api::V1::ProductsController < ApplicationController
 
   def total_price
     selected_products = params.fetch(:items, [])
-    if selected_products == ["GR1", "GR1"] 
-      render json: { total: "3.11€" }
-    elsif selected_products == ["SR1", "SR1", "GR1", "SR1"]
-      render json: { total: "16.61€" }
-    elsif selected_products == ["GR1", "CF1", "SR1", "CF1", "CF1"]
-      render json: { total: "30.57€" }
-    end
+    price_calculator = PriceCalculator.new(items: selected_products)
+    
+    render json: { total: price_calculator.total_price }
   end
 end
